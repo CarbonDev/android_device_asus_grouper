8a3285a312d1404b68fb5de9b1ed06b98378614c
winner00 <toverturf@gmail.com>
2013-06-11 11:43:55 -0600

    Add dalvik-heap file.
    Change-Id: I0f173ad1910bfcea33261c7c840b2009c519c5d2

--- device-common.mk
+++ device-common.mk
@@ -28,6 +28,8 @@ PRODUCT_PROPERTY_OVERRIDES := \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
 
+$(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
+
 PRODUCT_COPY_FILES += \
     device/asus/grouper/fstab.grouper:root/fstab.grouper \
     device/asus/grouper/ueventd.grouper.rc:root/ueventd.grouper.rc \
